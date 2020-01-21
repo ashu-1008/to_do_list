5dade410d9f6d59ee8d9ba36caf74002d2151f06
ashu-1008 <ashu-1008@users.noreply.github.com>
2020-01-22 01:53:30 +0530

    req

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn To_Do_App.wsgi
--- To_Do_App/settings.py
+++ To_Do_App/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
@@ -120,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+django_heroku.settings(locals())
